58941adb294763e6d647b06c7544ada844834c65
Mairondc21 <duartemairon@gmail.com>
2024-10-09 12:25:05 -0300

    criado

--- tests/teste_funcao.py
+++ tests/teste_funcao.py
@@ -2,5 +2,5 @@ from app.funcao import funcao_ola_turma
 
 def teste_ola_turma_retorna_ola_todos():
     saida = funcao_ola_turma()
-    gabarito = "ola tods"
+    gabarito = "ola todos"
     assert saida == gabarito
